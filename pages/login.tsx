59bd1d12f89c86b3a7d01c2f20057ff7478cde2c
Anibal <androso421@gmail.com>
2022-04-11 15:02:47 -0600

    adding nvmrc file

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function login() {
-  return (
-    <div>login</div>
-  )
+	return (
+		<div className="page-wrapper">
+			<h1>Schedule events between <span>timezones</span></h1>
+            <div>
+
+            </div>
+		</div>
+	);
 }
