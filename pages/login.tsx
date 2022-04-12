0108057ea4472918703509cca3433f377de58fab
Anibal <androso421@gmail.com>
2022-04-12 17:11:19 -0600

    adding supabase login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DiscordSVG, GoogleSVG } from "../components/icons";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN_DEV as string;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function login() {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		console.log(supabaseKey, "KEY");
+	}, [])
+
+	async function signInWithDiscord() {
+		//TODO IMPROVE THIS THING
+		const { user, session, error } = await supabase.auth.signIn({
+			provider: "discord",
+		}, {
+			redirectTo: `${DOMAIN}/dashboard`
+		})
+	}
+
 	return (
 		<div className="h-full flex flex-col items-center mx-auto w-[84%]">
 			<h1 className="title mt-40 text-4xl text-center font-bold font-sans max-w-[370px] lg:text-5xl lg:max-w-[480px] lg:mt-32">
@@ -11,15 +33,16 @@ export default function login() {
 				</span>
 			</h1>
 			<div className="auth-container mt-16 bg-containerGray mix-blend-normal py-16 w-full max-w-[430px] px-16 rounded-lg relative text-center drop-shadow-2xl">
-
 				<div className="auth-label absolute -top-5 left-1/2 -translate-x-1/2 px-4 bg-darkLabel py-2 rounded-md ">
-					<p className="bg-gradient-to-r from-lightBlue to-lightGreen text-transparent bg-clip-text font-semibold tracking-tighter">AUTHENTICATED</p>
+					<p className="bg-gradient-to-r from-lightBlue to-lightGreen text-transparent bg-clip-text font-semibold tracking-tighter">
+						AUTHENTICATED
+					</p>
 				</div>
 				<p className="indications font-medium text-lg max-w-[225px] mx-auto">
 					Log in to create and join events from friends
 				</p>
 				<div className="flex flex-col items-center mt-8 mx-auto">
-					<LoginButton>
+					<LoginButton onClick={signInWithDiscord}>
 						<span className="min-w-[35px] min-h-full flex items-center justify-center mr-2">
 							<DiscordSVG className="h-5" />
 						</span>
@@ -30,16 +53,16 @@ export default function login() {
 							<GoogleSVG className="h-5" />
 						</span>
 						<span className="font-semibold">GOOGLE</span>
-					</LoginButton>	
+					</LoginButton>
 				</div>
 			</div>
 		</div>
 	);
 }
 
-function LoginButton({ children }: any) {
+function LoginButton({ children, onClick }: any) {
 	return (
-		<button className="gradient-transition min-w-[180px] sm:min-w-[182px] py-4 lg:py-3 px-8 lg:px-7 flex items-center bg-gradient-to-t from-btnGradientBott to-btnGradientTop rounded-md mb-2 last:mb-0 drop-shadow-md">
+		<button onClick={onClick} className="gradient-transition cursor-pointer min-w-[180px] sm:min-w-[182px] py-4 lg:py-3 px-8 lg:px-7 flex items-center bg-gradient-to-t from-btnGradientBott to-btnGradientTop rounded-md mb-2 last:mb-0 drop-shadow-md">
 			{children}
 		</button>
 	);
