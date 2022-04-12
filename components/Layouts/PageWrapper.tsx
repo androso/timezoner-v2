e7f36b068693fc8b6d8423cb25d22de478aba844
Anibal <androso421@gmail.com>
2022-04-12 12:37:36 -0600

    adding transtitions to gradients

--- components/Layouts/PageWrapper.tsx
+++ components/Layouts/PageWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PageWrapper({children}:any) {
   return (
-    <div className='page-wrapper bg-gradient-to-t from-gunMetal to-blackCoral h-screen text-whiteText1 font-sans font-normal'>
+    <div className='page-wrapper bg-gradient-to-t from-gunMetal to-blackCoral min-h-screen text-whiteText1 font-sans font-normal'>
       {children}
     </div>
   )
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,19 +3,19 @@ import { DiscordSVG, GoogleSVG } from "../components/icons";
 
 export default function login() {
 	return (
-		<div className="h-full flex flex-col items-center mx-auto w-[85%]">
-			<h1 className="title mt-40 text-4xl text-center font-bold font-sans max-w-[370px]">
+		<div className="h-full flex flex-col items-center mx-auto w-[84%]">
+			<h1 className="title mt-40 text-4xl text-center font-bold font-sans max-w-[370px] lg:text-5xl lg:max-w-[480px] lg:mt-32">
 				Schedule events between{" "}
 				<span className="bg-gradient-to-r from-purpleGradientStart via-purpleGradientMid to-purpleGradientEnd text-transparent bg-clip-text">
 					timezones
 				</span>
 			</h1>
-			<div className="auth-container mt-16 bg-containerGray py-16 w-full max-w-[430px] px-16 rounded-lg relative text-center drop-shadow-2xl">
+			<div className="auth-container mt-16 bg-containerGray mix-blend-normal py-16 w-full max-w-[430px] px-16 rounded-lg relative text-center drop-shadow-2xl">
 
 				<div className="auth-label absolute -top-5 left-1/2 -translate-x-1/2 px-4 bg-darkLabel py-2 rounded-md ">
 					<p className="bg-gradient-to-r from-lightBlue to-lightGreen text-transparent bg-clip-text font-semibold tracking-tighter">AUTHENTICATED</p>
 				</div>
-				<p className="indications font-medium text-lg">
+				<p className="indications font-medium text-lg max-w-[225px] mx-auto">
 					Log in to create and join events from friends
 				</p>
 				<div className="flex flex-col items-center mt-8 mx-auto">
@@ -39,7 +39,7 @@ export default function login() {
 
 function LoginButton({ children }: any) {
 	return (
-		<button className="min-w-[180px] sm:min-w-[182px] py-4 px-8 flex items-center bg-gradient-to-t from-btnGradientBott to-btnGradientTop rounded-md mb-2 last:mb-0 drop-shadow-md">
+		<button className="gradient-transition min-w-[180px] sm:min-w-[182px] py-4 lg:py-3 px-8 lg:px-7 flex items-center bg-gradient-to-t from-btnGradientBott to-btnGradientTop rounded-md mb-2 last:mb-0 drop-shadow-md">
 			{children}
 		</button>
 	);
