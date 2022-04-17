a6572b82108d16d6b1a18e93a0b350eb274030b8
Anibal <androso421@gmail.com>
2022-04-16 18:29:30 -0600

    adding signOut function

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import { useContext } from "react";
 import { UserContext } from "../lib/context";
 import ProtectedRoute from "../components/ProtectedRoute";
-
+import { auth } from "../lib/firebase";
+import { signOut } from "firebase/auth";
 export default function Dashboard() {
 	const userData = useContext(UserContext);
+	const logOut = async () => {
+		try {
+			const result = await signOut(auth);
+			console.log("succesfully signed out!");
+		} catch(error) {
+			console.error(error);
+		}
 
+	}
 	return (
 		<div>
 			<ProtectedRoute options={{ pathAfterFailure: "/login" }}>
 					<h1>{userData?.user?.displayName}</h1>
 					<img src={userData?.user?.photoURL} />
+					<button onClick={logOut}>Sign Out</button>
 			</ProtectedRoute>
 		</div>
 	);
