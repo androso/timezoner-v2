67975b34078a87c326113dcfdde827812f05fa93
Anibal <androso421@gmail.com>
2022-05-06 08:04:22 -0600

    focused styles for title and description (mobile)
    '

--- components/CreateEventForm.tsx
+++ components/CreateEventForm.tsx
@@ -4,7 +4,9 @@ export default function CreateEventForm() {
 	return (
 		<form className="py-6 px-6 bg-gradient-to-b from-softBlackTransparent to-softBlackTransparent mx-auto rounded-md">
 			<h2 className=" font-semibold text-2xl mb-1">Create Event</h2>
-			<p className="font-medium text-shadowWhite mb-3">You’ll be able to invite your friends later</p>
+			<p className="font-medium text-shadowWhite mb-3">
+				You’ll be able to invite your friends later
+			</p>
 			<hr className="bg-[#666666] border-none h-[1px] rounded-sm mb-5" />
 			<EventFormFields />
 		</form>
@@ -14,27 +16,32 @@ export default function CreateEventForm() {
 function EventFormFields() {
 	return (
 		<>
-			<div>
-				<label className="block font-medium" htmlFor="event_title">
+			<div className="mb-4">
+				<label className="block font-medium text-lg" htmlFor="event_title">
 					Title
 				</label>
 				<input
-					className="w-full bg-deepBlack py-3 px-3 rounded-md border-solid border-[#4e4e4e] border-[1px] placeholder:font-medium "
+					className="w-full basic-input-field "
 					type="text"
 					name="event_title"
 					id="event_title"
 					placeholder="Event Title"
+					autoComplete="off"
 				/>
 			</div>
 			<div>
-				<label className="block" htmlFor="event_description">
+				<label
+					className="block font-medium text-lg"
+					htmlFor="event_description"
+				>
 					Description
 				</label>
 				<textarea
-					className="w-full bg-deepBlack"
+					className="w-full bg-deepBlack basic-input-field"
 					name="event_description"
 					id="event_description"
 					rows={2}
+					placeholder="Add a description..."
 				></textarea>
 			</div>
 			<div>
