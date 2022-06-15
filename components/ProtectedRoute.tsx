09dafe550d325cf7c6046e7945c99a70b648bb15
Anibal <androso421@gmail.com>
2022-06-15 06:52:08 -0600

    making optional to pass a path after failure

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import LoadingSpinner from "./LoadingSpinner";
-import { useRouter } from "next/router";
 import { UserContext } from "../lib/context";
 import { LoginForm } from "../components";
-
+import { useRouter } from "next/router";
 type props = {
 	children: any;
-	options: {
+	options?: {
 		pathAfterFailure: string;
 	};
 };
@@ -16,13 +15,15 @@ export default function ProtectedRoute({
 	options = { pathAfterFailure: "/login" },
 }: props) {
 	const { user, loading } = useContext(UserContext);
+	const router = useRouter();
 
 	if (loading) {
 		return <LoadingSpinner />;
 	} else if (user) {
 		return children;
+	} else if (options.pathAfterFailure) {
+		router.push(options.pathAfterFailure);
 	} else {
 		return <LoginForm />;
-		// router.push(options.pathAfterFailure, undefined, {shallow: true });
 	}
 }
