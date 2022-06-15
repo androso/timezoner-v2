35330d3f57f36cd7e934fbe5ac4bdfac3d3d526c
Anibal <androso421@gmail.com>
2022-06-15 16:49:50 -0600

    simplifying

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import LoadingSpinner from "./LoadingSpinner";
-import { UserContext } from "../lib/context";
+import { useAuth, UserContext } from "../lib/context";
 import { LoginForm } from "../components";
 import { useRouter } from "next/router";
+
 type props = {
 	children: any;
 	options?: {
@@ -14,15 +15,10 @@ export default function ProtectedRoute({
 	children,
 	options = { pathAfterFailure: "/login" },
 }: props) {
-	const { user, loading } = useContext(UserContext);
-	const router = useRouter();
-
-	if (loading) {
-		return <LoadingSpinner />;
-	} else if (user) {
+	// const { user, loading } = useContext(UserContext);
+	const { user, loading } = useAuth();
+	if (user) {
 		return children;
-	} else if (options.pathAfterFailure) {
-		router.push(options.pathAfterFailure);
 	} else {
 		return <LoginForm />;
 	}
