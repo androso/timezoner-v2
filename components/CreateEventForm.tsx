c76635bd891d2fa5158dfbb04f9cd4dc0d4f4b53
androso <androso421@gmail.com>
2022-06-21 18:12:49 -0600

    moving timezones-select to the createEventFormComponent

--- components/CreateEventForm.tsx
+++ components/CreateEventForm.tsx
@@ -1,30 +1,23 @@
 import dynamic from "next/dynamic";
 import "react-datepicker/dist/react-datepicker.css";
 import React from "react";
-import {
-	dateRange,
-	HourPickerProps,
-	inputProps,
-	EventFormValues,
-} from "../lib/utils/types";
+import type { dateRange, EventFormValues } from "../lib/utils/types";
 import { LightButton } from "./LightButton";
-import {
-	useForm,
-	Controller,
-	SubmitHandler,
-	UseFormRegister,
-	Control,
-	useController,
-} from "react-hook-form";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import Downshift from "downshift";
+import { defaultTimezone, timezonesLabels } from "../lib/timezonesData";
+import { matchSorter } from "match-sorter";
+import Input from "./Input";
+import Hourpicker from "./Hourpicker";
 
-const DynamicTimezonesSelect = dynamic(() => import("./TimezonesSelect"), {
-	ssr: false,
-	loading: () => <div className="basic-input-field w-full h-[50px]"></div>,
-});
 const DatePicker = dynamic(() => import("react-datepicker"), {
 	ssr: false,
 	loading: () => (
-		<input type="text" className="basic-input-field w-full"></input>
+		<input
+			type="text"
+			className="basic-input-field w-full placeholder:text-shadowWhite2"
+			placeholder="Select Date..."
+		></input>
 	),
 });
 
@@ -32,31 +25,17 @@ export default function CreateEventForm() {
 	const { register, handleSubmit, control } = useForm<EventFormValues>();
 	const submitForm: SubmitHandler<EventFormValues> = (data) =>
 		console.log(data);
-
 	return (
 		<form
 			onSubmit={handleSubmit(submitForm)}
 			className="py-6 px-6 bg-gradient-to-b from-softBlackTransparent to-softBlackTransparent mx-auto rounded-md max-w-lg"
+			autoComplete="off"
 		>
 			<h2 className=" font-semibold text-2xl mb-1">Create Event</h2>
 			<p className="font-medium text-shadowWhite mb-3">
-				You’ll be able to invite your friends later
+				You'll be able to invite your friends later
 			</p>
 			<hr className="bg-[#666666] border-none h-[1px] rounded-sm mb-5" />
-			<EventFormFields {...{ register, control }} />
-		</form>
-	);
-}
-
-function EventFormFields({
-	register,
-	control,
-}: {
-	register: UseFormRegister<EventFormValues>;
-	control: Control<EventFormValues, any>;
-}) {
-	return (
-		<>
 			<div className="mb-4">
 				<Input
 					placeholder="Event Title"
@@ -67,16 +46,95 @@ function EventFormFields({
 				/>
 			</div>
 			<div className="mb-2">
-				<TextArea
-					label="Description"
+				<label
+					className="block font-medium text-lg"
+					htmlFor="event_description"
+				>
+					Description
+				</label>
+				<textarea
+					className="w-full bg-deepBlack basic-input-field placeholder:text-shadowWhite2"
+					id="event_description"
+					rows={2}
 					placeholder="Add a description..."
 					required
-					register={register}
-					name="description"
-				/>
+					{...register("description", { required: true })}
+				></textarea>
 			</div>
 			<div className="mb-2">
-				<DynamicTimezonesSelect />
+				{/* <DynamicTimezonesSelect /> */}
+				<Downshift
+					itemToString={(item) => (item ? item : "")}
+					initialSelectedItem={defaultTimezone.label}
+				>
+					{({
+						getInputProps,
+						getItemProps,
+						getLabelProps,
+						getMenuProps,
+						getToggleButtonProps,
+						isOpen,
+						inputValue,
+						getRootProps,
+						highlightedIndex,
+					}) => (
+						<div className="relative">
+							<label {...getLabelProps()} className="text-lg">
+								Timezone
+							</label>
+
+							<div
+								/* 
+									// @ts-ignore */
+								{...getRootProps({}, { suppressRefError: true })}
+								className="flex"
+							>
+								<input
+									{...getInputProps()}
+									className="basic-input-field grow rounded-r-none rounded-br-none border-r-0"
+								/>
+								<button
+									aria-label={"toggle menu"}
+									className="px-3 bg-deepBlack border-solid border-[#4e4e4e] border-[1px] rounded-r-md rounded-br-md"
+									type="button"
+									{...getToggleButtonProps()}
+								>
+									{isOpen ? <span>&#8593;</span> : <span>&#8595;</span>}
+								</button>
+							</div>
+							<ul
+								{...getMenuProps()}
+								className={`${
+									isOpen ? "visible" : "invisible"
+								} mt-1 max-h-48 w-full overflow-y-scroll absolute z-10 bg-deepBlack border-solid border-[#415d95] border-[1px]`}
+							>
+								{isOpen
+									? (inputValue != "" && inputValue != null
+											? matchSorter(timezonesLabels, inputValue, {
+													threshold: matchSorter.rankings.CONTAINS,
+											  })
+											: timezonesLabels
+									  ).map((item, index) => {
+											return (
+												<li
+													{...getItemProps({
+														key: item,
+														index,
+														item,
+													})}
+													className={`px-4 py-3 ${
+														highlightedIndex === index ? "bg-[#393d3f]" : null
+													}`}
+												>
+													{item}
+												</li>
+											);
+									  })
+									: null}
+							</ul>
+						</div>
+					)}
+				</Downshift>
 			</div>
 			<div className="mb-2">
 				<label htmlFor="event_date" className="block text-lg font-medium">
@@ -89,17 +147,8 @@ function EventFormFields({
 					defaultValue={[null, null]}
 					render={({ field }) => {
 						const [startingDate, endingDate] = field.value;
-						const { ref } = field;
-						//TODO find a reason to why Datepicker doens't accept the field object with field.value inside when doing {...field} in Datepicker props
-						const fieldObjectWithoutValue = {
-							name: field.name,
-							ref,
-							onChange: field.onChange,
-							onBlur: field.onBlur,
-						};
 						return (
 							<DatePicker
-								{...fieldObjectWithoutValue}
 								selected={startingDate}
 								onChange={(dates: dateRange) => {
 									field.onChange(dates);
@@ -121,7 +170,7 @@ function EventFormFields({
 			</div>
 			<div className="flex mb-6">
 				<div>
-					<HourPicker
+					<Hourpicker
 						label="From"
 						placeholder="Start"
 						required
@@ -130,7 +179,7 @@ function EventFormFields({
 					/>
 				</div>
 				<div>
-					<HourPicker
+					<Hourpicker
 						label="To"
 						placeholder="End"
 						required
@@ -142,83 +191,6 @@ function EventFormFields({
 			<div className="w-full text-center">
 				<LightButton innerText="CREATE EVENT" css="mx-auto" btnType="submit" />
 			</div>
-		</>
-	);
-}
-
-function Input({ label, placeholder, required, register, name }: inputProps) {
-	return (
-		<>
-			<label className="block font-medium text-lg" htmlFor="event_title">
-				{label}
-			</label>
-			<input
-				className="w-full basic-input-field placeholder:text-shadowWhite2"
-				type="text"
-				id="event_title"
-				placeholder={placeholder}
-				autoComplete="off"
-				required={required ? true : false}
-				{...register(name, { required })}
-			/>
-		</>
-	);
-}
-
-function TextArea({
-	label,
-	placeholder,
-	required,
-	register,
-	name,
-}: inputProps) {
-	return (
-		<>
-			<label className="block font-medium text-lg" htmlFor="event_description">
-				{label}
-			</label>
-			<textarea
-				className="w-full bg-deepBlack basic-input-field placeholder:text-shadowWhite2"
-				id="event_description"
-				rows={2}
-				{...{ required, placeholder }}
-				{...register(name, { required })}
-			></textarea>
-		</>
-	);
-}
-function HourPicker({
-	label,
-	placeholder,
-	required,
-	control,
-	name,
-}: HourPickerProps) {
-	const ControllerProps = {
-		control,
-		name,
-		defaultValue: undefined,
-	};
-	const {
-		field: { onBlur, onChange, value, ref },
-	} = useController(ControllerProps);
-
-	return (
-		<>
-			<label className="block text-lg font-medium" htmlFor="date_from">
-				{label}
-			</label>
-			<DatePicker
-				{...{ name, onBlur, onChange, ref, required }}
-				selected={value}
-				showTimeSelect
-				showTimeSelectOnly
-				timeIntervals={30}
-				timeCaption="Time"
-				dateFormat="h:mm aa"
-				className="basic-input-field max-w-[120px] placeholder:text-shadowWhite2 mr-4"
-				placeholderText={placeholder}
-			/>
-		</>
+		</form>
 	);
 }
---
+++ lib/timezonesData.ts
@@ -0,0 +1,20 @@
+import { timeZonesNames } from "@vvo/tzdb";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const userTimezoneDefault = dayjs.tz.guess();
+
+export const defaultTimezone = {
+	label: userTimezoneDefault.replace(/_/g, " "),
+	value: userTimezoneDefault,
+};
+export const timezonesOptionsObjects = timeZonesNames.map((tz) => ({
+	value: tz,
+	label: tz.replace(/_/g, " "),
+}));
+
+export const timezonesLabels = timezonesOptionsObjects.map((tz) => tz.label);
