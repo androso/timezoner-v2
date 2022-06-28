5d2a63ef558326ab5f0f4915c186db2913ab46e8
androso <androso421@gmail.com>
2022-06-28 15:17:51 -0600

    adding useAsync hook

---
+++ lib/utils/hooks/useAsync.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import type { Reducer } from "react";
+
+enum AsyncActionType {
+	loading = "login",
+	resolved = "resolved",
+	rejected = "rejected",
+}
+type StatusTypes = "idle" | "loading" | "resolved" | "rejected";
+
+type AsyncReducerState = {
+	status: StatusTypes;
+	data: unknown | null;
+	error: Error | null;
+};
+
+type AsyncAction =
+	| { type: AsyncActionType.loading }
+	| { type: AsyncActionType.resolved; data: unknown }
+	| { type: AsyncActionType.rejected; error: Error };
+
+type InitialStateProps =
+	| {
+			status: "idle" | "loading" | "resolved" | "rejected";
+			data: unknown | null;
+			error: Error | null;
+	  }
+	| {};
+
+const asyncReducer: Reducer<AsyncReducerState, AsyncAction> = (
+	state,
+	action
+) => {
+	switch (action.type) {
+		case AsyncActionType.loading: {
+			return { status: "loading", data: null, error: null };
+		}
+		case AsyncActionType.resolved: {
+			return { status: "resolved", data: action.data, error: null };
+		}
+		case AsyncActionType.rejected: {
+			return { status: "rejected", data: null, error: action.error };
+		}
+		default: {
+			throw new Error(`Unhandled action type`);
+		}
+	}
+};
+
+const useAsync = (initialState: InitialStateProps = {}) => {
+	const [state, dispatch] = React.useReducer(asyncReducer, {
+		status: "idle",
+		data: null,
+		error: null,
+		...initialState,
+	});
+
+	const { data, error, status } = state;
+
+	const run = React.useCallback((promise: Promise<unknown>) => {
+		dispatch({ type: AsyncActionType.loading });
+		promise.then(
+			(data: unknown) => dispatch({ type: AsyncActionType.resolved, data }),
+			(error: Error) => dispatch({ type: AsyncActionType.rejected, error })
+		);
+	}, []);
+
+	return {
+		error,
+		status,
+		data,
+		run,
+	};
+};
+
+export default useAsync;
