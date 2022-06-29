390b8fc439abf9e79d7af2a1260f705b59bbb5a4
androso <androso421@gmail.com>
2022-06-29 06:54:19 -0600

    displaying error message when rejected

--- pages/event/[eventId].tsx
+++ pages/event/[eventId].tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { doc, getDoc } from "firebase/firestore";
-import { firestore } from "../../lib/firebase";
 import ProtectedRoute from "../../components/ProtectedRoute";
 import Header from "../../components/Header";
 import Container from "../../components/Layouts/Container";
@@ -10,23 +7,12 @@ import useParsedUserData from "../../lib/utils/hooks/useParsedUserData";
 import type { UserData } from "../../lib/utils/types";
 import useEventData from "../../lib/utils/hooks/useEventData";
 
-/* We're gonna need
-   - Event data
-   - User data (avatar)
-*/
 export default function eventId() {
 	const { parsedUser } = useParsedUserData();
 	const [organizerData, setOrganizerData] = React.useState<UserData>();
-	const { eventData, status, error} = useEventData();
+	const { eventData, status, error } = useEventData();
 
-	React.useEffect(() => {
-		console.log("first time rendering event overview !")
-	}, [])
-
-	if (status === "loading" || status === "idle") {
-
-	}
-	if (status === "resolved") {
+	if (status === "resolved" || status === "loading" || status === "idle") {
 		return (
 			<ProtectedRoute>
 				<Header
@@ -44,12 +30,15 @@ export default function eventId() {
 			</ProtectedRoute>
 		);
 	}
+
 	if (status === "rejected") {
-		console.log("marking this as an error");
 		return (
 			<ProtectedRoute>
-				<p className="text-red-200">Sorry, something went wrong with your request, try reloading the page</p>
+				<p className="text-yellow-400">
+					Sorry, something went wrong with your request, try reloading the page
+				</p>
+				<pre>{error ? JSON.stringify(error) : null}</pre>
 			</ProtectedRoute>
-		)
+		);
 	}
 }
