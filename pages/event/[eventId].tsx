4ad969bbf94e6c59802da58829d7c476e4964da8
androso <androso421@gmail.com>
2022-06-29 07:03:45 -0600

    using data from firestore

--- pages/event/[eventId].tsx
+++ pages/event/[eventId].tsx
@@ -3,13 +3,9 @@ import ProtectedRoute from "../../components/ProtectedRoute";
 import Header from "../../components/Header";
 import Container from "../../components/Layouts/Container";
 import HomeBreadcrumbs from "../../components/HomeBreadcrumbs";
-import useParsedUserData from "../../lib/utils/hooks/useParsedUserData";
-import type { UserData } from "../../lib/utils/types";
 import useEventData from "../../lib/utils/hooks/useEventData";
 
 export default function eventId() {
-	const { parsedUser } = useParsedUserData();
-	const [organizerData, setOrganizerData] = React.useState<UserData>();
 	const { eventData, status, error } = useEventData();
 
 	if (status === "resolved" || status === "loading" || status === "idle") {
@@ -18,12 +14,12 @@ export default function eventId() {
 				<Header
 					title={eventData?.title ?? undefined}
 					screenName="EVENT"
-					photoURL={parsedUser?.photoURL ?? undefined}
+					photoURL={eventData?.organizer_data.avatar_url ?? undefined}
 				/>
 				<Container className="pt-4 sm:pt-6">
 					<HomeBreadcrumbs currentPage="Event" />
 					<h1 className="mb-5">
-						Welcome to {organizerData?.username.split(" ")[0]}'s Event
+						Welcome to {eventData?.organizer_data.username}'s Event
 					</h1>
 					<p>{eventData?.description ?? "..."}</p>
 				</Container>
