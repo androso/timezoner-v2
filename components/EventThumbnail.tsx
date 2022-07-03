138c9a281f82e0fa39773cde0884f19b2d749fb1
androso <androso421@gmail.com>
2022-07-03 13:20:06 -0600

    adding arrow icon

--- components/EventThumbnail.tsx
+++ components/EventThumbnail.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { EventData } from "../lib/utils/types";
-
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //TODO: WORK ON HOW TO FORMAT CORRECTLY THE DATE RANGE
 const getFormattedDate = (date: Date) => {
 	const allMonths = [
@@ -38,14 +39,17 @@ export default function EventThumbnail({
 			<a className={`${css}`}>
 				<div className=" flex py-6 px-4 rounded-md bg-gradient-to-t from-eventGradientBott to-eventGradientTop">
 					<div className="mr-2 relative h-16 w-16">
-						<Image src={eventData.organizer_data.avatar_url} layout="fill" />
+						<Image src={eventData.organizer_data.avatar_url} layout="fill" className="rounded-full"/>
 					</div>
-					<div className="flex flex-col justify-center">
+					<div className="flex flex-col justify-center grow">
 						<h6 className="font-semibold text-xl">{eventData.title}</h6>
 						<p className="font-medium text-secondaryWhite">
-							{startDate} - {endDate}
+							{startDate === endDate ? startDate : `${startDate} - ${endDate }`} 
 						</p>
 					</div>
+					<span className="self-center mr-2 text-secondaryWhite">
+						<FontAwesomeIcon icon={faChevronRight} className="h-4"/>
+					</span>
 				</div>
 			</a>
 		</Link>
