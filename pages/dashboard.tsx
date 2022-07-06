c2bc91fe82a1ed6100dbcf1d667fc1da571682d8
androso <androso421@gmail.com>
2022-07-06 13:42:29 -0600

    adding disabled state to btn when no more events to fetch

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,18 +6,15 @@ import useAllUserEvents from "../lib/utils/hooks/useAllUserEvents";
 import EventThumbnail from "../components/EventThumbnail";
 import {
 	collection,
-	getDoc,
 	getDocs,
 	limit,
 	orderBy,
 	query,
 	startAfter,
-	startAt,
 	where,
 } from "firebase/firestore";
 import { firestore } from "../lib/firebase";
 import { getUserEventsData } from "../lib/utils/client-helpers";
-import _isEqual from "lodash.isequal";
 
 const Container = dynamic(() => import("../components/Layouts/Container"), {
 	ssr: false,
@@ -39,15 +36,10 @@ const LightButton = dynamic<BtnProps>(
 	() => import("../components/LightButton").then((md) => md.LightButton),
 	{ ssr: false }
 );
-const UpcomingEvents = dynamic(() => import("../components/UpcomingEvents"), {
-	ssr: false,
-});
 
 export default function Dashboard() {
 	//! use collection of events where the user is either the organizer of the event or a participant.
-	// We could fetch two pages (10 documents) but only render the first 5 of them.
-	// We show the next button if there are more than 5 documents in the allEvents state,
-	// if not, it means this is the last page
+	// currently we're only fetching events organized
 
 	const { parsedUser } = useParsedUserData();
 	const {
@@ -62,10 +54,6 @@ export default function Dashboard() {
 		eventsPageIndex * 5,
 		eventsPageIndex * 5 + 5
 	);
-
-	React.useEffect(() => {
-		// console.log(allEvents);
-	});
 
 	return (
 		<ProtectedRoute>
@@ -96,9 +84,13 @@ export default function Dashboard() {
 						<p className="self-center mr-2 ">{eventsPageIndex + 1}</p>
 						<button
 							className={`p-4 bg-gray-800 rounded-md disabled:bg-gray-500`}
-							// disabled={!lastDocSnap && (eventsPageIndex === Math.floor(allEvents?.length / 5) -1) ? true : false}
+							disabled={
+								allEvents &&
+								eventsPageIndex + 1 === Math.ceil(allEvents?.length / 5)
+									? true
+									: false
+							}
 							onClick={async () => {
-								//! WORKING ON:
 								if (allEvents) {
 									const currentIndex = eventsPageIndex + 1; // so that we start from 1 instead of 0
 									const numberOfPagesAvailable = Math.ceil(
@@ -108,16 +100,12 @@ export default function Dashboard() {
 										numberOfPagesAvailable - 1 === currentIndex;
 
 									if (!lastDocSnap && currentIndex === numberOfPagesAvailable) {
-										//! this fires if you go to the last page, then back 1, then try to go next
-										console.log(
-											"button next should be disabled, find a way"
-										);
+										console.log("button next should be disabled, find a way");
 										return;
 									}
 
 									if (isPageSecondToLast && lastDocSnap) {
 										// we fetch
-										// console.log("we fetch mijo");
 										if (parsedUser) {
 											const eventsQuery = query(
 												collection(firestore, "events"),
@@ -151,66 +139,14 @@ export default function Dashboard() {
 										}
 									}
 									// we update the index by default
-									setEventsPageIndex(prevIndex => prevIndex + 1);
+									setEventsPageIndex((prevIndex) => prevIndex + 1);
 								}
-
-								// 	if (
-								// 		eventsPageIndex + 1 ===
-								// 		Math.ceil(allEvents.length / 5) - 1
-								// 	) {
-								// 		// console.log("we should fetch new next batch of events!");
-
-								// 		if (parsedUser) {
-								// 			const eventsQuery = query(
-								// 				collection(firestore, "events"),
-								// 				where("organizer_id", "==", parsedUser.id),
-								// 				orderBy("date_range.start_date"),
-								// 				startAfter(lastDocSnap),
-								// 				limit(10)
-								// 			);
-
-								// 			const snapshot = await getDocs(eventsQuery);
-								// 			const {
-								// 				participatingEvents: nextBatchOfEvents,
-								// 				lastEventSnapshot,
-								// 			} = await getUserEventsData(snapshot);
-								// 			console.log({ nextBatchOfEvents });
-								// 			setAllEvents((prevEvents) => {
-								// 				if (prevEvents) {
-								// 					return [...prevEvents, ...nextBatchOfEvents];
-								// 				} else {
-								// 					return [...nextBatchOfEvents];
-								// 				}
-								// 			});
-								// 			setEventsPageIndex((prevIndex) => prevIndex + 1);
-								// 			setLastDocSnap(lastEventSnapshot);
-								// 			console.log("we just fetched a new batch of events !");
-								// 		}
-								// 	} else {
-								// 		console.log("updating index");
-								// 		setEventsPageIndex((prevIndex) => prevIndex + 1);
-								// 	}
-								// }
-								// if (!lastDocSnap) {
-								// 	console.log(" no more events!");
-								// 	return;
-								// }
 							}}
 						>
 							Next
 						</button>
 					</div>
 					<ul>
-						{/* {allEventsStatus === "success" &&
-							allEvents &&
-							allEvents?.map((event, index) => {
-								return (
-									<li key={event.id} className="mb-3 relative">
-										<EventThumbnail css="mb-2" eventData={event} />
-									</li>
-								);
-							})} */}
-
 						{allEventsStatus === "success" &&
 							currentPageEvents &&
 							currentPageEvents.map((event, index) => {
